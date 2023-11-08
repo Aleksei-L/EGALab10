cbf410062f7a3adfb41e44af4a94302c8ec4e7a0
Aleksei Lobanov <lobanov782@gmail.com>
2023-11-08 21:05:23 +0300

    Added selection_beta_tournament() operator for selection Added selection_proportional() operator for selection Added selection_linear_ranking() operator for selection Edited main loop

--- main.py
+++ main.py
@@ -174,6 +174,58 @@ def genotype_modification(N, w_max, things, person):
 	return person_copy
 
 
+# Отбирает g особей для переноса в следующее поколение
+def selection_beta_tournament(new_population_size, new_population, g):
+	g_to_new_population = []
+	for i in range(0, g):
+		# TODO Как мы выбираем чему равна бета?
+		beta = random.randint(2, new_population_size // 2)
+		tournament = random.sample(new_population, beta)
+		fun_maxim_fitness_person = 0
+		fun_maxim_person = tournament[0]
+		for j in range(0, len(tournament)):
+			fun_fit = get_fitness(N, things, tournament[j])
+			if fun_fit > fun_maxim_fitness_person:
+				fun_maxim_fitness_person = fun_fit
+				fun_maxim_person = tournament[j]
+		g_to_new_population.append(fun_maxim_person.copy())
+	return g_to_new_population
+
+
+# Отбор особей при помощи рулетки
+# TODO Почему в этой селекции одна и та же особь может быть выбрана несколько раз?
+# TODO Сколько селекция должна возвращать особей? g или ню
+def selection_proportional(new_population_size, new_population, g):
+	g_to_new_population = []
+	prices = []
+	for i in range(0, new_population_size):
+		prices.append((i, get_fitness(N, things, new_population[i])))
+
+	for i in range(0, g):
+		item = roulette(prices)
+		g_to_new_population.append(new_population[item[0]].copy())
+
+	return g_to_new_population
+
+
+def selection_linear_ranking(new_population_size, new_population, g):
+	g_to_new_population = []
+	new_population_copy = deepcopy(new_population)
+	for i in range(0, new_population_size):
+		for j in range(i + 1, new_population_size):
+			if get_fitness(N, things, new_population_copy[i]) > get_fitness(N, things, new_population_copy[j]):
+				new_population_copy[i], new_population_copy[j] = new_population_copy[j], new_population_copy[i]
+	prices = []
+	for i in range(0, new_population_size):
+		prices.append((i, i + 1))
+
+	for i in range(0, g):
+		item = roulette(prices)
+		g_to_new_population.append(new_population_copy[item[0]].copy())
+
+	return g_to_new_population
+
+
 # Получение приспособленности кодировки
 def get_fitness(N, things, person):
 	res = 0.0
@@ -202,8 +254,8 @@ operator_choices = [
 	int(input(
 		"Оператор формирования начальной популяции:\n1) Случайный\n2) Случайный с контролем\n3) Жадный алгоритм\n>> ")),
 	int(input("Оператор кроссовера:\n1) Одноточечный\n2) Двуточечный\n3) Однородный\n>> ")),
-	int(input("Оператор мутации:\n1) Точечная\n2) Макромутация инверсией\n3) Хромосомная\n>> "))
-	# int(input("Оператор селекции:\n1)\n2)\n>> "))
+	int(input("Оператор мутации:\n1) Точечная\n2) Макромутация инверсией\n3) Хромосомная\n>> ")),
+	int(input("Оператор селекции:\n1) Бета-турнир\n2) Пропорциональная\n3) Линейная ранговая\n>> "))
 ]
 
 # Создание начальной популяции
@@ -215,7 +267,10 @@ elif operator_choices[0] == 2:
 elif operator_choices[0] == 3:
 	population = create_population_greedy_algo(N, w_max, things, population_size)
 
-for t in range(0, 5):
+solution = 0
+t = 1
+without_change_solution_counter = 0
+while True:
 	# Вывод всех особей поколения и наилучшей особи
 	print("Поколение", t)
 	print("Все особи:")
@@ -238,7 +293,6 @@ for t in range(0, 5):
 	# TODO Стратегия выбора родителей: рандом или мнение пользователя? (Пока рандом)
 	# Рандомный выбор стратегии отбора родителей
 	rand_choice = random.randint(1, 2)
-	print("Рандомная стратегия") if rand_choice == 1 else print("Рулеточная стратегия")
 	for i in range(0, population_size):
 		parent1, parent2 = [], []
 		if rand_choice == 1:  # Рандомный выбор родителей
@@ -291,21 +345,69 @@ for t in range(0, 5):
 		if get_weight(N, things, new_population[i]) > w_max:
 			new_population[i] = genotype_modification(N, w_max, things, new_population[i])
 
-	# ...
+	# Подготовливаем следующее поколение
 	next_population = []
 
-	# Среди потомков и мутантов принудительно копируем самую лучшую особь в следующее поколение
+	# Получение коэффициента перекрытия поколений и кол-ва особей для замены
+	G = round(random.uniform(0.01, 1), 2)
+	g = round(G * population_size)
+
+	# Среди текущего поколения и репродуктивного множества скопируем самую лучшую особь в следующее поколение
 	maxim_fitness_person = 0
 	maxim_person = new_population[0]
+	flag = 0
 	for i in new_population:
 		fit = get_fitness(N, things, i)
 		if fit > maxim_fitness_person:
 			maxim_fitness_person = fit
 			maxim_person = i
-	next_population.append(maxim_person)
-	new_population.remove(maxim_person)
+			flag = 1
+	for i in population:
+		fit = get_fitness(N, things, i)
+		if fit > maxim_fitness_person:
+			maxim_fitness_person = fit
+			maxim_person = i
+			flag = 2
+	next_population.append(maxim_person.copy())
 
-	# Принудительно скопируем самую лучшую особь в следующее поколение
+	# TODO Кол-во потомков может быть больше кол-ва предков?
+	# if flag == 1:
+	# 	new_population.remove(maxim_person)
+	# elif flag == 2:
+	# 	population.remove(maxim_person)
+
+	# Равновероятный отбор g особей из старой популяции для дальнейшей замены
+	g_from_population = random.sample(population, g)
+
+	# Удаляем всех отобранных особей из популяции, чтобы быстрее скопировать особи в следующее поколение
+	# TODO Удалять их сейчас или потом?
+	for i in g_from_population:
+		population.remove(i)
+
+	# Копируем всех остальных особей в следующее поколение
+	for i in population:
+		next_population.append(i.copy())
+
+	# Применяем селекцию для отбора из репродуктивного множества g особей в следующее поколение
+	# TODO Правильно ли подсчитаны числа?
+	#  1 -> лучшая особь из поколения и репродуктивного множества
+	#  |P^t| - g = (population_size - 1) - g -> текущая популяция без самого лучшего и g особей под замену
+	#  |R^t| = g -> g особей отобранных из репродуктивного множества
+	to_new_population = []
+	if operator_choices[3] == 1:
+		to_new_population = selection_beta_tournament(len(new_population), new_population, g)
+	elif operator_choices[3] == 2:
+		to_new_population = selection_proportional(len(new_population), new_population, g)
+	elif operator_choices[3] == 3:
+		to_new_population = selection_linear_ranking(len(new_population), new_population, g)
+
+	for i in range(0, g):
+		next_population.append(to_new_population[i].copy())
+
+	# Смена поколения
+	population = deepcopy(next_population)
+
+	# Проверим изменилось ли решение
 	maxim_fitness_person = 0
 	maxim_person = population[0]
 	for i in population:
@@ -313,16 +415,15 @@ for t in range(0, 5):
 		if fit > maxim_fitness_person:
 			maxim_fitness_person = fit
 			maxim_person = i
-	next_population.append(maxim_person)
-	population.remove(maxim_person)
 
-	# Получение коэффициента перекрытия поколений и кол-ва особей для замены
-	G = round(random.uniform(0.01, 1), 2)
-	g = round(G * population_size)
+	if maxim_fitness_person != solution:
+		solution = maxim_fitness_person
+	else:
+		without_change_solution_counter += 1
 
-	# Равновероятный отбор g особей из старой популяции для дальнейшей замены
-	g_from_population = random.sample(population, g)
+	if without_change_solution_counter >= 10:
+		print("Конец эволюции")
+		break
 
-	# TODO Удалять их сейчас или потом?
-	for i in g_from_population:
-		population.remove(i)
+	t += 1
+	print()
